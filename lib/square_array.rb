4c55b5e9b2c1ded8c7f961b9bc3e4633696f5312
placid-tier-4550 <james.ardery@gmail.com>
2020-09-21 18:36:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  counter = 0
+  square_array = []
+  while numbers[counter] <= numbers.length do
+    square_array << numbers[counter] ** numbers[counter]
+  end
+  puts square_array
 end
